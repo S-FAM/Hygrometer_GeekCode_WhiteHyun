22263cce9329b14db693dbbca9244fa736817c2b
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-07-10 00:44:41 +0900

    Feat: PageViewController 추가

--- HygrometerApp/Sources/ViewControllers/MainViewController.swift
+++ HygrometerApp/Sources/ViewControllers/MainViewController.swift
@@ -7,8 +7,24 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 final class MainViewController: UIViewController {
- 
+    
+    // MARK: - Properties
+    
+    /// 지역별 습도를 보여주는 VC를 갖고있는 pageViewController 입니다.
+    /// 양옆으로 스와이프하여 `viewController`이동이 가능하도록 설계되었습니다.
+    private lazy var pageViewController = UIPageViewController(
+        transitionStyle: .scroll,
+        navigationOrientation: .horizontal
+    ).then {
+        $0.setViewControllers([CityViewController()], direction: .forward, animated: true)
+    }
+    
+    // MARK: - Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
