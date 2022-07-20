38bf8053c4eb90bc4b07dd6fd52c15d3d087adbe
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-07-20 15:49:30 +0900

    Refactor: 기존 GPS API 모델 삭제

--- HygrometerApp/Sources/GpsParamModel.swift
+++
@@ -1,8 +0,0 @@
-@available(iOS, deprecated, renamed: "GpsRequestModel")
-struct GpsParamModel: Codable {
-  let key, query, request, type, category: String
-}
-
-struct WeatherParamModel: Codable {
-    let lat, long, appid, lang: String
-}
