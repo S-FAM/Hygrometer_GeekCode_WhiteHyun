794ac3ada117a681430df3bf0870df318d8af026
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-07-22 14:21:21 +0900

    Refactor: xib 삭제로인한 이니셜라이저 재설정

--- HygrometerApp/Sources/ViewControllers/TableViewCells/WeatherListTableViewCell.swift
+++ HygrometerApp/Sources/ViewControllers/TableViewCells/WeatherListTableViewCell.swift
@@ -8,16 +8,12 @@
 import UIKit
 
 class WeatherListTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
