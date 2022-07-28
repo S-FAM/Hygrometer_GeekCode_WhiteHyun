218b92ec2f282183c34f3767186b736b7343c701
isGeekcode <ptuguitar@naver.com>
2022-07-28 21:09:22 +0900

    feat: ListVC 에 ListTableView생성

--- HygrometerApp/Sources/ViewControllers/ListViewController.swift
+++ HygrometerApp/Sources/ViewControllers/ListViewController.swift
@@ -4,13 +4,49 @@
 //
 //  Created by 홍승현 on 2022/07/09.
 //
-
+import SnapKit
 import UIKit
 
 class ListViewController: UIViewController {
     
+    //MARK: - Properties
+    var weatherModel: WeatherRequest?
+    var weatherList: WeatherResponse?
+    var userData: UserDataModel?
+    
+    lazy var weatherListTableView = UITableView().then {
+        $0.backgroundColor = .clear
+        $0.keyboardDismissMode = .onDrag
+        $0.dataSource = self
+        $0.delegate = self
+        $0.register(CityListTableViewCell.self, forCellReuseIdentifier: "CityListTableViewCell")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .systemTeal
     }
+}
+
+// MARK: - UITableViewDelegate
+extension ListViewController: UITableViewDelegate {
+ 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension ListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherListTableViewCell", for: indexPath) as! WeatherListTableViewCell
+        
+        cell.locationNameLabel.text = ""
+        cell.humidityLabel.text = ""
+        return cell
+    }
+    
 }
--- HygrometerApp/Sources/ViewControllers/TableViewCells/WeatherListTableViewCell.swift
+++ HygrometerApp/Sources/ViewControllers/TableViewCells/WeatherListTableViewCell.swift
@@ -9,12 +9,25 @@ import UIKit
 
 class WeatherListTableViewCell: UITableViewCell {
     
+    let containerView = UIView()
+    let locationNameLabel = UILabel()
+    let humidityLabel = UILabel()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setWeatherListTabeViewCellLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setWeatherListTabeViewCellLayout() {
+        self.addSubview(containerView)
+        containerView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.85)
+        }
     }
 }
