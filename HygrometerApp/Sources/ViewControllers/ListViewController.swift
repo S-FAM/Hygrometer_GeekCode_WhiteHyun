bf6def7025b80d6d9a6fa8ca155056a1c8b0622e
isGeekcode <ptuguitar@naver.com>
2022-08-04 23:20:06 +0900

    Feat: EmptyCell 구분하여 세팅
    - 모델의 마지막은 빈 값으로 생성했으나 reuse때문에 스크롤시 빈칸이 생겨나는 이슈발생
    - 따로 emptyCell을 구현하여 마지막 IndexPath에 세팅 하였습니다.

--- HygrometerApp/Sources/ViewControllers/ListViewController.swift
+++ HygrometerApp/Sources/ViewControllers/ListViewController.swift
@@ -18,6 +18,7 @@ class ListViewController: UIViewController {
         $0.dataSource = self
         $0.delegate = self
         $0.register(WeatherListTableViewCell.self, forCellReuseIdentifier: "WeatherListTableViewCell")
+        $0.register(ListTableEmptyCell.self, forCellReuseIdentifier: "ListTableEmptyCell")
     }
 
     override func viewDidLoad() {
@@ -62,38 +63,46 @@ extension ListViewController: UITableViewDelegate {
 extension ListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherListTableViewCell", for: indexPath) as! WeatherListTableViewCell
-
-        if indexPath.row == UserData.shared.items.count {
-            cell.humidityLabel.isHidden = true
-            cell.locationNameLabel.isHidden = true
-            cell.plusView.isHidden = false
+        let isLastCell = indexPath.row == viewModel.models.count
+        
+        switch isLastCell {
+            
+        case true:
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ListTableEmptyCell", for: indexPath) as! ListTableEmptyCell
             return cell
+            
+        case false:
+            
+            let model = viewModel.models[indexPath.row]
+            let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherListTableViewCell", for: indexPath) as! WeatherListTableViewCell
+            cell.configure(with: model)
+            
+            return cell
+
         }
-        
-        let model = viewModel.models[indexPath.row]
-        cell.configure(with: model)
-        cell.plusView.isHidden = true
-        
-        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if indexPath.row == UserData.shared.items.count {
+        let isLastCell = indexPath.row == UserData.shared.items.count
+        
+        switch isLastCell {
+            
+        case true:
             
             let vc = AddViewController()
             navigationController?.pushViewController(vc, animated: true)
-        } else {
+            
+        case false:
+            
             let alertVC = UIAlertController(title: "", message: "삭제하시겠습니까?", preferredStyle: .alert)
             let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
-            let confirm = UIAlertAction(title: "확인", style: .default, handler: nil)
-            [
-                cancel,
-                confirm
-            ].forEach {
-                alertVC.addAction($0)
+            let confirm = UIAlertAction(title: "확인", style: .default) { [weak self] UIAlertAction in
+                UserData.shared.items.remove(at: indexPath.row )
+                self?.weatherListTableView.reloadData()
             }
+            [ cancel, confirm ].forEach { alertVC.addAction($0) }
             self.present(alertVC, animated: true, completion: nil)
         }
     }
