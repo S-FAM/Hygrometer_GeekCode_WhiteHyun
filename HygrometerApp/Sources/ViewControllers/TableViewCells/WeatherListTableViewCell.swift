93b11c2471e54265cd82c65952070e63f6cbe030
isGeekcode <ptuguitar@naver.com>
2022-08-04 23:01:26 +0900

    Feat: WeatherListTableViewCell 리팩토링, plusView  삭제

--- HygrometerApp/Sources/ViewControllers/TableViewCells/WeatherListTableViewCell.swift
+++ HygrometerApp/Sources/ViewControllers/TableViewCells/WeatherListTableViewCell.swift
@@ -9,14 +9,22 @@ import UIKit
 
 class WeatherListTableViewCell: UITableViewCell {
     
-    let containerView = UIView()
     let humidityLabel = UILabel()
     let locationNameLabel = UILabel()
-    let plusView = UIImageView()
+
+    lazy var containerView = UIView().then {
+        $0.backgroundColor = .themeColor
+        $0.layer.cornerRadius = 20
+        $0.layer.shadowColor = ShadowSet.shadowColor
+        $0.layer.shadowOffset = ShadowSet.shadowOffsetStrong
+        $0.layer.shadowRadius = ShadowSet.shadowRadius
+        $0.layer.shadowOpacity = ShadowSet.shadowOpacityWeak
+        $0.layer.masksToBounds = false
+    }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setWeatherListTabeViewCellLayout()
+        setLayout()
         setDetail()
     }
     
@@ -25,57 +33,40 @@ class WeatherListTableViewCell: UITableViewCell {
     }
     
     func setDetail(){
-        containerView.backgroundColor = .themeColor
-        containerView.layer.cornerRadius = 20
-        //그림자
-        containerView.layer.shadowColor = ShadowSet.shadowColor
-        containerView.layer.shadowOffset = ShadowSet.shadowOffsetStrong
-        containerView.layer.shadowRadius = ShadowSet.shadowRadius
-        containerView.layer.shadowOpacity = ShadowSet.shadowOpacityWeak
-        containerView.layer.masksToBounds = false
-        
-        humidityLabel.text = "40%"
-        humidityLabel.textColor = .white
+        backgroundColor = .clear
+        selectionStyle = .none
+
         humidityLabel.font = UIFont.boldSystemFont(ofSize: 40)
-        
-        locationNameLabel.text = "수원시"
-        locationNameLabel.textColor = .white
         locationNameLabel.font = UIFont.boldSystemFont(ofSize: 15)
+
+        [ humidityLabel, locationNameLabel ].forEach {
+            $0.text = ""
+            $0.textColor  = .white
+        }
         
-        let image = UIImage(systemName: "plus")
-        plusView.image = image
-        plusView.backgroundColor = .clear
-        plusView.layer.cornerRadius = 20
-        plusView.tintColor = .systemGray4
-        plusView.contentMode = .scaleAspectFit
     }
     
-    func setWeatherListTabeViewCellLayout() {
-        self.contentView.addSubview(containerView)
-        [locationNameLabel, humidityLabel, plusView].forEach {
-            containerView.addSubview($0)
+    func setLayout() {
+        contentView.addSubview(containerView)
+        containerView.addSubview(locationNameLabel)
+        containerView.addSubview(humidityLabel)
+        
+        containerView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.centerY.equalToSuperview()
+            $0.height.equalToSuperview().multipliedBy(0.85)
         }
         
-        containerView.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().inset(20)
-            make.centerY.equalToSuperview()
-            make.height.equalToSuperview().multipliedBy(0.85)
+        humidityLabel.snp.makeConstraints {
+            $0.top.equalTo(containerView).offset(30)
+            $0.leading.equalTo(containerView).offset(20)
+            $0.width.equalTo(containerView).multipliedBy(0.4)
         }
         
-        plusView.snp.makeConstraints { make in
-            make.edges.equalTo(containerView).inset(30)
-        }
-        
-        humidityLabel.snp.makeConstraints { make in
-            make.top.equalTo(containerView).offset(30)
-            make.leading.equalTo(containerView).offset(20)
-            make.width.equalTo(containerView).multipliedBy(0.4)
-        }
-        
-        locationNameLabel.snp.makeConstraints { make in
-            make.top.equalTo(humidityLabel.snp.bottom).offset(20)
-            make.leading.equalTo(containerView).offset(20)
-            make.width.equalTo(200)
+        locationNameLabel.snp.makeConstraints {
+            $0.top.equalTo(humidityLabel.snp.bottom).offset(20)
+            $0.leading.equalTo(containerView).offset(20)
+            $0.width.equalTo(200)
         }
     }
     
