246d07c6bc6024acf6a8f4f8c58e6df517033769
isGeekcode <ptuguitar@naver.com>
2022-08-04 23:56:37 +0900

    Refactor: 콤바인 리팩토링 반영

--- HygrometerApp/Sources/ViewControllers/KeyboardMonitor.swift
+++ HygrometerApp/Sources/ViewControllers/KeyboardMonitor.swift
@@ -8,70 +8,29 @@
 import Combine
 import UIKit
 
-final class KeyboardMonitor: ObservableObject {
+final class KeyboardMonitor {
     
     enum Status {
-        case show, hide
-        var description: String {
-            switch self {
-            case .show: return "보임"
-            case .hide: return "안보임"
-            }
-        }
+        case show
+        case hide
     }
     
     var subscriptions = Set<AnyCancellable>()
     
-    @Published var updatedKeyboardStatusAction: Status = .hide
-    @Published var keyboardHeight: CGFloat = 0.0
-    
-    func keyboardMonitorNoti(noti: Notification) {
-        print("KeyboardMonitor - keyboardWillShowNotification: noti: \(noti)") //Here
-    }
+    @Published var status: Status = .hide
     
     init() {
         
         // 키보드가 올라올 때 이벤트가 들어옴
-        NotificationCenter.Publisher(center: .default, name: UIResponder.keyboardWillShowNotification) // 아래 값을 구독
+        NotificationCenter.Publisher(center: .default, name: UIResponder.keyboardWillShowNotification)
             .sink { [weak self] noti in
-                self?.keyboardMonitorNoti(noti: noti)
-                self?.updatedKeyboardStatusAction = .show
+                self?.status = .show
             }.store(in: &subscriptions)
         
         // 키보드가 내려갈 때 이벤트가 들어옴
         NotificationCenter.Publisher(center: .default, name: UIResponder.keyboardWillHideNotification)
             .sink { [weak self] noti in
-                self?.keyboardMonitorNoti(noti: noti)
-                self?.updatedKeyboardStatusAction = .hide
-            }.store(in: &subscriptions)
-        
-        // 키보드 크기가 변경될 때 이벤트가 들어옴
-        NotificationCenter.Publisher(center: .default, name: UIResponder.keyboardWillChangeFrameNotification)
-            .sink { [weak self] noti in
-                self?.keyboardMonitorNoti(noti: noti)
-                let keyboardFrame = noti.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
-                self?.keyboardHeight = keyboardFrame.height
-            }.store(in: &subscriptions)
-        
-        /// 키보드 올라온 이벤트 처리 -> 키보드 높이
-        NotificationCenter.Publisher(center: .default, name: UIResponder.keyboardWillShowNotification)
-            .merge(with: NotificationCenter.Publisher(center: .default, name: UIResponder.keyboardWillChangeFrameNotification))
-            .compactMap { noti in
-                return noti.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect
-            }.map { keyboardFrame in
-                return keyboardFrame.height
-            }
-            .sink { [weak self] height in
-                self?.keyboardHeight = height
-            }.store(in: &subscriptions)
-        
-        /// 키보드 내려갈때 이벤트 처리 -> 키보드 높이
-        NotificationCenter.Publisher(center: .default, name: UIResponder.keyboardWillHideNotification)
-            .compactMap { noti in
-                return .zero
-            }
-            .sink { [weak self] height in
-                self?.keyboardHeight = height
+                self?.status = .hide
             }.store(in: &subscriptions)
     }
 }
