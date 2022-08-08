e0f1f401ec73448dd1cf6719b04269d745250c69
Hyeonseok Bang <76529148+isGeekCode@users.noreply.github.com>
2022-08-08 23:08:43 +0900

    Update HygrometerApp/Sources/ViewControllers/TableViewCells/WeatherListTableViewCell.swift
    Co-authored-by: 홍승현 <whi7ehyun@gmail.com>

--- HygrometerApp/Sources/ViewControllers/TableViewCells/WeatherListTableViewCell.swift
+++ HygrometerApp/Sources/ViewControllers/TableViewCells/WeatherListTableViewCell.swift
@@ -32,7 +32,7 @@ class WeatherListTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setDetail(){
+    func setDetail() {
         backgroundColor = .clear
         selectionStyle = .none
 
