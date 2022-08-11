110f3fcf0dbf4553999d8ce1787e7e013d65e42a
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-08-11 17:46:55 +0900

    Fix: Extension 누락 수정
    - UIImage extension이 사라져있는 것 수정

---
+++ HygrometerApp/Sources/Extensions/UIImage+backgroundImage.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+ extension UIImage {
+     static func backgroundImage(with humidity: PhraseModel) -> UIImage? {
+         switch humidity {
+         case .dry:
+             return UIImage(named: "dry")
+         case .normal:
+             return UIImage(named: "normal")
+         case .humid:
+             return UIImage(named: "humid")
+         }
+     }
+ }
